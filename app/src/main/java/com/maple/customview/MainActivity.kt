51d91b96cd0eba1bdc6aa141facdb077869fe549
Maple Shao <939078792@qq.com>
2019-09-04 10:56:47 +0800

    add value pack View.

--- app/src/main/java/com/maple/customview/MainActivity.kt
+++ app/src/main/java/com/maple/customview/MainActivity.kt
@@ -5,6 +5,9 @@ import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.maple.customview.adapter.BaseQuickAdapter
+import com.maple.customview.adapter.ValuePackAdapter
+import com.maple.customview.adapter.ValuePackBean
 import com.maple.customview.databinding.ActivityMainBinding
 
 /**
@@ -15,6 +18,16 @@ import com.maple.customview.databinding.ActivityMainBinding
  */
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+    private val valuePackAdapter by lazy {
+        ValuePackAdapter(this).apply {
+            onItemClickListener = object : BaseQuickAdapter.OnItemClickListener {
+                override fun onItemClick(view: View, position: Int) {
+
+                }
+            }
+        }
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,12 +37,29 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initView() {
-        binding.tbTopBar.apply {
-            setTitle("new Title")
-            setLeftButtonText("Back")
-            setOnLeftButtonClickListener(View.OnClickListener {
-                Toast.makeText(this@MainActivity, "back", Toast.LENGTH_SHORT).show()
-            })
+        binding.apply {
+            tbTopBar.apply {
+                setTitle("new Title")
+                setLeftButtonText("Back")
+                setOnLeftButtonClickListener(View.OnClickListener {
+                    Toast.makeText(this@MainActivity, "back", Toast.LENGTH_SHORT).show()
+                })
+            }
+            rvRecords.apply {
+                adapter = valuePackAdapter
+            }
         }
+
+        valuePackAdapter.refreshData(getTestData())
     }
+
+
+    fun getTestData(): List<ValuePackBean> {
+        return arrayListOf(
+                ValuePackBean("单次", "0.01", "0.01", 2),
+                ValuePackBean("包月", "0.02", "0.02", 30),
+                ValuePackBean("俩月", "0.03", "0.03", 60)
+        )
+    }
+
 }
---
+++ app/src/main/java/com/maple/customview/adapter/ValuePackAdapter.kt
@@ -0,0 +1,53 @@
+package com.maple.customview.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.maple.customview.R
+import com.maple.customview.databinding.ViewValuePackBinding
+import java.io.Serializable
+
+/**
+ * https://github.com/CymChad/BaseRecyclerViewAdapterHelper
+ *
+ * @author maple
+ * @time 2019-08-06
+ */
+class ValuePackAdapter(val mContext: Context)
+    : BaseQuickAdapter<ValuePackBean, RecyclerView.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val binding = DataBindingUtil.inflate<ViewValuePackBinding>(
+                LayoutInflater.from(mContext), R.layout.view_value_pack, parent, false)
+        return ValuePackViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        (holder as ValuePackViewHolder).bind(getItem(position))
+    }
+
+    inner class ValuePackViewHolder(val binding: ViewValuePackBinding)
+        : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item: ValuePackBean) {
+            bindViewClickListener(this)
+            binding.apply {
+                tvType.text = item.type
+                tvNewValue.text = "¥ ${item.newValue}"
+                tvOldValue.text = "¥ ${item.oldValue}"
+                tvTime.text = "有效时间${item.time}天"
+            }
+        }
+    }
+}
+
+data class ValuePackBean(
+        var type: String,
+        var newValue: String,
+        var oldValue: String,
+        var time: Int = 0
+) : Serializable
+
+
